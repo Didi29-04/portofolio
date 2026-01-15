427af7588ec4bc966db7cc36d5474f4a8ed6f73a
unknown <alfidianxannov@gmail.com>
2026-01-15 22:38:54 +0700

    Revisi sedikit App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import About from './components/About';
-import ProjectCard from './components/ProjectCard';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
-import Cursor from './components/Cursor';
-import ScrollToTop from './components/ScrollToTop';
-import { projects } from './data/projects';
-import './App.css'; 
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import ProjectCard from "./components/ProjectCard";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import Cursor from "./components/Cursor";
+import ScrollToTop from "./components/ScrollToTop";
+import { projects } from "./data/projects";
+import "./App.css";
 
 function App() {
   return (
@@ -19,12 +19,15 @@ function App() {
       <ScrollToTop />
       <section id="projects" className="projects-section">
         <div className="container">
-            <h2>Portofolio Project</h2>
-            <div className="grid">
+          <h2
+            style={{ textAlign: "center", marginBottom: "3rem", fontSize: "2.5rem",}}
+          >Portofolio Project</h2>
+
+          <div className="grid">
             {projects.map((project) => (
-                <ProjectCard key={project.id} project={project} />
+              <ProjectCard key={project.id} project={project} />
             ))}
-            </div>
+          </div>
         </div>
       </section>
       <Contact />
